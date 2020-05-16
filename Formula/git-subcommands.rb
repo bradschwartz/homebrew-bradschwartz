9839758deef93619eb501321ca937ef794158ec2
bradschwartz <baschwartz95@gmail.com>
2020-05-15 21:11:25 -0500

    fix brew style issues

--- Formula/git-subcommands.rb
+++ Formula/git-subcommands.rb
@@ -1,13 +1,11 @@
 class GitSubcommands < Formula
   desc "Subcommands for git"
   homepage "https://github.com/bradschwartz/bash"
-  version "0.0.1"
-  url "https://github.com/bradschwartz/git-subcommands/archive/#{version}.tar.gz"
+  url "https://github.com/bradschwartz/git-subcommands/archive/0.0.1.tar.gz"
   sha256 "da32d3eecbbc31589b69f27d13bd7d4823ac5ee2a4629fd7ba6b8e2241e40d44"
 
   def install
-      bin.install 'git-home'
-      bin.install 'git-org-clone'
+    bin.install "git-home"
+    bin.install "git-org-clone"
   end
 end
-  
